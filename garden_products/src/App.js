474dbd3383434e71d797bf3169140b92b8df401d
Mikita.Asipenka <sandra.sanya@mail.ru>
2024-03-20 22:33:01 +0300

    components and styles

--- garden_products/src/App.js
+++ garden_products/src/App.js
@@ -1,23 +1,10 @@
 import './App.css';
-import NavMenu from './components/NavMenu';
-import { Routes, Route } from 'react-router-dom';
-import MainPage from './pages/MainPage';
-import CategoriesPage from './pages/CategoriesPage';
-import AllProductsPage from './pages/AllProductsPage';
-import AllSalesPage from './pages/AllSalesPage';
+import Header from './components/Header';
 
 function App() {
   return (
     <div>
-      <p>
-        <NavMenu />
-        <Routes>
-          <Route path='/' element={<MainPage />} />
-          <Route path='/categories' element={<CategoriesPage />} />
-          <Route path='/products' element={<AllProductsPage />} />
-          <Route path='/sales' element={<AllSalesPage />} />
-        </Routes>
-      </p>
+      <Header />
     </div>
   );
 }
