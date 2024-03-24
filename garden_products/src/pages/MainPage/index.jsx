f2b6d94c9722fe6bbefe9cfe0196e1c9911ae978
Zarina.Guriyeva <guriyeva@gmail.com>
2024-03-24 17:33:45 +0100

    create Header + banner with button(change to sale page)

--- garden_products/src/pages/MainPage/index.jsx
+++ garden_products/src/pages/MainPage/index.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Footer from '../../components/Footer'
+import Main from '../../components/Main'
 
 export default function MainPage() {
   return (
     <div>
+      <Main />
       <Footer />
     </div>
   )
