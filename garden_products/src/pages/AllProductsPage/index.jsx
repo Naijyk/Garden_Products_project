dbe398627f2af5cedda2c208529a2b04283f15f8
Zarina.Guriyeva <guriyeva@gmail.com>
2024-04-09 00:14:34 +0200

    Sort, AllSalesPage

--- garden_products/src/pages/AllProductsPage/index.jsx
+++ garden_products/src/pages/AllProductsPage/index.jsx
@@ -3,6 +3,7 @@ import { getAllProducts } from '../../requests/allProducts'
 import { useDispatch, useSelector } from 'react-redux'
 import ProductCard from '../../components/ProductCard';
 import s from './index.module.css'
+import { sortProductsAction } from '../../store/reducers/allProductsReducer';
 
 export default function AllProductsPage({id, image, product}) {
 
@@ -12,12 +13,32 @@ export default function AllProductsPage({id, image, product}) {
   }, [])
 
   const allProductsData = useSelector(store => store.allProducts);
+  
+
+  const order = event => {
+    dispatch(sortProductsAction(event.target.value))
+  }
 
   return (
-        <div className={[s.products_page, 'wrapper'].join(' ')}>
-          {
+        
+        <div className={['wrapper', s.products_page].join(' ')}>
+          <p>All products</p>
+          <label> Discounted items <input type="checkbox"/></label>
+          <div className={s.sort_products}>
+            <span>Sorted:</span>
+            <select onInput={order}>
+              <option>by default</option>
+              <option value='price_desc'>by descending price</option>
+              <option value='price_asc'>by ascending price</option>
+              <option value='alphabetically'>alphabetically</option>
+            </select>
+          </div>
+          <div className={s.products_card}>
+            {
             allProductsData.map(el => <ProductCard key={el.id} {...el}/>)
-          }
+            } 
+          </div>
+         
         </div>
   )
 }
--- garden_products/src/store/reducers/allProductsReducer.js
+++ garden_products/src/store/reducers/allProductsReducer.js
@@ -1,12 +1,25 @@
 const defaultState = [];
 
 const LOAD_ALL_PRODUCTS = 'LOAD_ALL_PRODUCTS';
+const SORT_PRODUCTS = 'SORT_PRODUCTS';
+
+
 
 export const loadAllProductsAction = allProducts => ({ type:LOAD_ALL_PRODUCTS,payload: allProducts });
+export const sortProductsAction = (value) => ({type: SORT_PRODUCTS, payload: value});
 
 export const allProductsreducer = (state = defaultState, action) => {
     if( action.type === LOAD_ALL_PRODUCTS ){
         return action.payload
+    } else if(action.type === SORT_PRODUCTS){
+        if(action.payload === 'price_asc'){
+            state.sort((a, b) => a.price - b.price)
+        }else if(action.payload === 'alphabetically'){
+            state.sort((a, b) => a.title.localeCompare(b.title))
+        }else if(action.payload === 'price_desc'){
+            state.sort((a, b) => b.price - a.price)
+        }
+        return [...state]
     }
 
     return state
